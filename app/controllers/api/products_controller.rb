11073861b4b7222ba2aed7105beca83222b70bf4
Ali Bagcioglu <alikadir.bagcioglu@gusto.com>
2022-03-29 23:07:27 +0300

    crud - update - delete

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -11,8 +11,30 @@ module Api
       render json: @product
     end
 
+    def update
+      being
+
+      rescue
+      @product = Product.find(params[:id])
+      # @product = Product.find(id: params[:id]) # you can also write the id parameter
+      @product.update(product_params)
+      render json: @product
+    end
+
+    def destroy
+      @product = Product.find_by_id(params[:id])
+      if @product
+        @product.destroy
+        render json: @product
+      else
+        render :json => "PRODUCT NOT FOUND!"
+      end
+    end
+
     def product_params
-      params.permit(:name, :description, :quantity, :price)
+      @my_params = params.permit(:name, :description, :quantity, :price)
+      pp @my_params
+      @my_params
     end
   end
 end
